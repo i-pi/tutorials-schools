1ba89a5500f08a512494841c96c9c6c9b03130e6
Venkat Kapil <venkat.kapil@gmail.com>
2021-05-18 16:17:03 +0100

    replaced scripts with symlinks

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 f2py --fcompiler=gfortran --f90flags=-ffree-line-length-0 -m fortran -c fortran.f90 
-cp fortran.cpython*so ../day-1 
+cd ../day-1/
+ln -s ../tools/fortran.cpython*so .
+cd ../day-2/
+ln -s ../tools/fortran.cpython*so .
+cd ../tools/
