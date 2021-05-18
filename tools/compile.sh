35c572c908b0c43299ba07703e97d71af7e1d6c1
Venkat Kapil <venkat.kapil@gmail.com>
2021-05-18 15:14:10 +0100

    bugfix in compile.sh

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 f2py --fcompiler=gfortran --f90flags=-ffree-line-length-0 -m fortran -c fortran.f90 
-cp fortran.cpython*so ../tutorial-1 
-cp fortran.cpython*so ../tutorial-2 
+cp fortran.cpython*so ../day-1 
