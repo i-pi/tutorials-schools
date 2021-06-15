44dd3e07f08af7640fe018f4f323b034afdab2de
PIMD-16-TUTORIAL <pimd16@cosmo>
2021-06-15 16:22:48 +0200

    Links the fortran static objects to the tutorial directories

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 f2py --fcompiler=gfortran --f90flags=-ffree-line-length-0 -m fortran -c fortran.f90 
-cd ../day-1/
+cd ../tutorial-1/
 ln -s ../tools/fortran.cpython*so .
-cd ../day-2/
+cd ../tutorial-2/
+ln -s ../tools/fortran.cpython*so .
+cd ../tutorial-3/
+ln -s ../tools/fortran.cpython*so .
+cd ../tutorial-4/
 ln -s ../tools/fortran.cpython*so .
 cd ../tools/
